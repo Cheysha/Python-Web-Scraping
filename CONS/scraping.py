a476ebcb2bea4a88a86bcee82e3d9d6533103b02
chey <hyman.sha95@gmail.com>
2023-05-17 14:52:57 -0500

    set up for multiple scrapers

--- CONS/scraping.py
+++ CONS/scraping.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 # setup the page archive and the page counter
 
-subject = "walmart_rx"
+subject = "aarp"
 pages = []
 number_of_pages = 1
 current_page = 1
@@ -11,16 +11,18 @@ current_page = 1
 # run until we get a page that is not valid
 
 while True:
+    url = f"https://www.consumeraffairs.com/rx/{subject}.html?page={current_page}"
+
     # get page
-    page = requests.get(f"https://www.consumeraffairs.com/rx/{subject}.html?page={current_page}", headers={'User-Agent': 'Mozilla/5.0'})
+    page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
     # get the returned url
     returned_url = page.url
 
-    # escape condish
-    if (returned_url == f"https://www.consumeraffairs.com/rx/aarp.html?page={current_page-1}"):
+    # escape condition, also, 1st page does not abide by the rules
+    if (returned_url != url and current_page != 1):
         break
-    if (current_page == 10):
+    if (current_page == 100):
         break
 
     # put page in our page archive
@@ -31,7 +33,7 @@ while True:
 
 # dump eaach page to a file in the Archive folder, if the folder does not exist it will be created
 for i in range(len(pages)):
-    with open(f"Archive/{subject}_{i}.html", "w",encoding="utf-8") as f:
+    with open(f"../Archive/ConsumerAffairs/{subject}_{i}.html", "w",encoding="utf-8") as f:
         f.write(pages[i].text)
 
 # print out some info
