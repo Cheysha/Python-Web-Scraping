4e6296d1ba4308bbb870f59cdf1ec1761abb6f8d
chey <hymanc@student.swosu.edu>
2023-06-12 18:39:41 -0500

    code cleanup

--- RateMyPerfessor/RMPSCRAPER.py
+++ RateMyPerfessor/RMPSCRAPER.py
@@ -13,33 +13,48 @@ review_dataframes = pd.DataFrame(columns=['ID', 'Quality', 'Difficulty', 'Class_
 
 
 def make_driver():
-    # Create Driver and set options
-    # option = webdriver.ChromeOptions()
+    # Create driver options
     option = webdriver.FirefoxOptions()
     option.add_argument("--headless")
-    # option.add_argument("log-level=3")
     option.add_argument("--disable-extensions")
     # option.page_load_strategy = "eager"
     option.add_argument("--no-sandbox")
     option.add_argument("--disable-gpu")
 
     # Create Driver, install uBlock Origin, and return
-    # driver = webdriver.Chrome(options=option)
     driver = webdriver.Firefox(options=option)
     driver.install_addon('uBlock.xpi', temporary=True)
     return driver
+
+def process_teachers(chunk):
+    # Create Driver
+    driver = make_driver()
+    print("driver created, getting page")
+
+    # loop through the teachers, and get their reviews, reusing the driver
+    try:
+        for index, row in chunk.iterrows():
+            teacher = row['link']
+            print("getting reviews for", teacher)
+            try:
+                get_teacher_reviews(teacher, review_dataframes, driver)
+            except Exception as e:
+                print("error getting reviews for", teacher, e)
+    finally:
+        driver.close()
+
 def get_university_teacher_list(university_id):
     # Create Driver and setup
     driver = make_driver()
     counter = 0
     max_page_count = 1
     max_teacher_count = 3
-    print("driver created, getting page")
 
     # Get page
     url = f"https://www.ratemyprofessors.com/search/professors/{university_id}?q=*"
+    print("getting page " + url)
     driver.get(url)
-    print("got page")
+    print("got page + " + url)
 
     # Close cookie warning if it exists
     if len(driver.find_elements(By.XPATH, "/html/body/div[5]/div/div/button")) > 0:
@@ -58,12 +73,14 @@ def get_university_teacher_list(university_id):
             time.sleep(1)
             counter += 1
             print("clicked show more button " + str(counter))
-            if (counter >= max_page_count):  # TESTING BREAK
+
+            # safe way out
+            if (counter >= max_page_count):
                 break
         except NoSuchElementException:
             break
 
-
+    # reassure the user everything is all right
     print("finished loading page")
 
     # now that we've loaded all teachers on page, Scrape the page for teachers
@@ -80,15 +97,14 @@ def get_university_teacher_list(university_id):
 
     # Create dataframe from teacher list
     for teacher in teacher_list_elements:
-        data = {"link": teacher[0], "name": teacher[1][3], "school": teacher[1][5], "department": teacher[1][4],
-                "rating": teacher[1][1], "difficulty": teacher[1][8], "would_take_again": teacher[1][6]}
-        t = pd.DataFrame(data, index=[0])
         teacher_dataframes.loc[len(teacher_dataframes)] = [teacher[0], teacher[1][3], teacher[1][5], teacher[1][4],
                                                            teacher[1][1], teacher[1][8], teacher[1][6]]
     # Close driver, on to part 2!
     driver.close()
+
 def get_teacher_reviews(teacher_url, review_frame, driver):
-    # Create Driver
+    # get page
+    print("getting page for", teacher_url)
     driver.get(teacher_url)
     print("got page for", teacher_url)
 
@@ -101,15 +117,16 @@ def get_teacher_reviews(teacher_url, review_frame, driver):
 
     # Click load more button until there are no more reviews
     while True:
-        load_more_button = driver.find_elements(By.XPATH, "/html/body/div[2]/div/div/div[3]/div[4]/div/div/button")
-        if len(load_more_button) > 0:
+        try:
+            load_more_button = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div[3]/div[4]/div/div/button")
             # scroll unitl the button is in the middle of the screen
-            # driver.execute_script("arguments[0].scrollIntoView({ block: 'center', inline: 'center'})", load_more_button)
-            load_more_button[0].click()
+            driver.execute_script("arguments[0].scrollIntoView({ block: 'center', inline: 'center'})", load_more_button)
+            load_more_button.click()
             time.sleep(2)
             print("clicked load more button")
-        else:
+        except NoSuchElementException:
             break
+
     print("finished hitting load more ratings")
 
     # try scraping the page for reviews
@@ -165,22 +182,6 @@ def get_teacher_reviews(teacher_url, review_frame, driver):
                                                          attendance,
                                                          grade, would_take_again, for_credit, tags,
                                                          review_string]  # add after daate
-def process_teachers(chunk):
-    # Create Driver
-    driver = make_driver()
-    print("driver created, getting page")
-
-    # loop through the teachers, and get their reviews, reusing the driver
-    try:
-        for index, row in chunk.iterrows():
-            teacher = row['link']
-            print("getting reviews for", teacher)
-            try:
-                get_teacher_reviews(teacher, review_dataframes, driver)
-            except Exception as e:
-                print("error getting reviews for", teacher, e)
-    finally:
-        driver.close()
 
 
 if __name__ == '__main__':
@@ -190,12 +191,12 @@ if __name__ == '__main__':
     print(teacher_dataframes.to_string())
     print("got teacher list ", len(teacher_dataframes), " gettting reviews")
 
-    # split the teacher dataframes into chunks, setup the threads
+    # split the teacher dataframes into n chunks
     n = 4
     chunks = np.array_split(teacher_dataframes, n)
-    threads = []
 
     # create the threads
+    threads = []
     for i in range(n):
         threads.append(threading.Thread(target=process_teachers, args=(chunks[i],)))
 
@@ -207,6 +208,7 @@ if __name__ == '__main__':
     for thread in threads:
         thread.join()
 
+    # reassure the user that everything is okay
     print("finished getting reviews")
 
     # print the dataframes
@@ -215,4 +217,3 @@ if __name__ == '__main__':
     # Exporting the dataframes collected to csv
     teacher_dataframes.to_csv(f'./Data/{school_id}teacher_dataframes.csv', index=False)
     review_dataframes.to_csv(f'./Data/{school_id}review_dataframes.csv', index=False)
-    # rocess_teachers exports its own dataframe, beacause it is threaded
